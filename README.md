a98a7e911a145e7c382f23d8be2251186cc9a8e4
mika6 <xlyao_666@163.com>
2021-03-19 11:10:21 +0800

    add readme

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # vite-test
+
+## install
+```shell
+yarn install 
+```
+
+## dev
+```shell
+yarn run dev
+```
+
+## build
+```shell
+yarn run build
+```
+
--- packages/demo/src/index.ts
+++ packages/demo/src/index.ts
@@ -4,7 +4,11 @@ const demo = {
   dvs
 }
 
-export {
-  demo
-}
+const div = document.createElement("div");
+div.innerText = "hello div element";
+div.style.background = "#095762";
+div.style.width = "100%";
+div.style.height = "100px";
+document.body.appendChild(div);
+
 
